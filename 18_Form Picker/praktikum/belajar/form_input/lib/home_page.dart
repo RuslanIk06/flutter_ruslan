5a4df579bc489861904f255fa6488e1715991357
RuslanIk06 <ruslan.andev@gmail.com>
2022-09-28 11:57:49 +0700

    Tugas Section 18 - Form Picker

---
+++ 18_Form Picker/praktikum/belajar/form_input/lib/home_page.dart
@@ -0,0 +1,151 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:open_file/open_file.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  DateTime _dueDate = DateTime.now();
+  final currentDate = DateTime.now();
+  Color _currentColor = Colors.orange;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Interactive Widget"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: ListView(
+          children: [
+            // build picker
+            buildDatePicker(context),
+            SizedBox(height: 20),
+            // buildcolorpicker
+            buildColorPicker(context),
+            //buildFilePicker
+            builFilePicker(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildDatePicker(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Date"),
+            TextButton(
+                onPressed: () async {
+                  final selectDate = await showDatePicker(
+                    context: context,
+                    initialDate: currentDate,
+                    firstDate: DateTime(1990),
+                    lastDate: DateTime(currentDate.year + 5),
+                  );
+
+                  setState(() {
+                    if (selectDate != null) {
+                      _dueDate = selectDate;
+                    }
+                  });
+                },
+                child: Text("Select"))
+          ],
+        ),
+        Text(
+          DateFormat("dd-MM-yyyy").format(_dueDate),
+        ),
+      ],
+    );
+  }
+
+  Widget buildColorPicker(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text("Color"),
+        SizedBox(height: 10),
+        Container(
+          height: 100,
+          width: double.infinity,
+          color: _currentColor,
+        ),
+        SizedBox(height: 20),
+        Center(
+          child: ElevatedButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text("Pick Your Color"),
+                    content: BlockPicker(
+                        pickerColor: _currentColor,
+                        onColorChanged: (color) {
+                          setState(() {
+                            _currentColor = color;
+                          });
+                        }),
+                    actions: [
+                      TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text("Save"))
+                    ],
+                  );
+                },
+              );
+            },
+            child: Text("Pick Color"),
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(_currentColor)),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget builFilePicker(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Pick File"),
+        SizedBox(height: 10),
+        Center(
+          child: ElevatedButton(
+            onPressed: () {
+              _pickFile();
+            },
+            child: Text("Pick and Open File"),
+          ),
+        )
+      ],
+    );
+  }
+
+  void _pickFile() async {
+    final result = await FilePicker.platform.pickFiles();
+    if (result == null) return;
+
+    final file = result.files.first;
+    _openFile(file);
+  }
+
+  void _openFile(PlatformFile file) {
+    OpenFile.open(file.path);
+  }
+}
---
+++ 18_Form Picker/readme.md
@@ -0,0 +1,21 @@
+# (18) Form Picker
+
+## Data Diri
+
+Nomor Urut : 1_023FLB_52
+Nama : Ruslan
+
+## Summary
+
+Di section ini saya belajar tentang Form Picker yang meliputi :
+
+- Interactive Widget
+  Interactive widget adalah interaksi antara aplikasi dan user yang mengumpulkan input dan feedback dari user. berikut beberapa contoh interactive widget :
+
+  - Date Picker
+    Date Picker adalah widget yang dimana user bisa memasukan tanggal. contoh penerapan date picker pada aplikasi adalah saat menentukan waktu pemesanan tiket, waktu reservasi, jdwal meeting dll. Untuk membuat date picker bisa menggunakan fungsi bawaan dari flutter showDatePicker yang memiliki tipe data future sehingga menggunakan async await, selain itu diperlukan sebuah package intl.
+
+  - Color Picker
+    Color Picker merupakan widget yang bisa memilih color yang dimana pengunaannya bisa menggunakan berbagai macam kondisi dan untuk membuat color picker diperlukan sebuah package flutter_colorpicker
+  - File Picker
+    File Picker adalah kemampuan suatu widget untuk emngakses storege dari HP yang bisa membuka dan memilih file, untuk membuat file picker diperlukan 2 buah packaga yaitu yang pertama adalah package file_picker untuk mengakses storage dari HP kita dan package yang kedua adalah open_file yang digunakan untuk membuka file yang yang dipilih.
