11882a58b1f3a75e5da8c431b87f8b68ba6ab595
RuslanIk06 <ruslan.andev@gmail.com>
2022-10-07 13:21:41 +0700

    Tugas Section 23 - Flutter Animation

---
+++ 23_Flutter Animation/praktikum/belajar/flutter_animation/lib/home_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animation/about_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isbig = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Screen"),
+      ),
+      body: Center(
+          child: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                isbig = !isbig;
+              });
+            },
+            child: Text("Resize"),
+          ),
+          Container(
+            width: isbig ? 200 : 100,
+            height: isbig ? 200 : 100,
+            color: Colors.amber,
+          ),
+          SizedBox(height: 20),
+          AnimatedContainer(
+            width: isbig ? 200 : 100,
+            height: isbig ? 200 : 100,
+            color: Colors.amber,
+            duration: Duration(milliseconds: 300),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                PageRouteBuilder(
+                  pageBuilder: (context, animation, secondaryAnimation) {
+                    return AboutScreen();
+                  },
+                  transitionsBuilder:
+                      ((context, animation, secondaryAnimation, child) {
+                    // final tween = Tween(begin: Offset(0, .5), end: Offset.zero);
+                    // return SlideTransition(
+                    //   position: animation.drive(tween),
+                    //   child: child,
+                    // );
+
+                    // final tween = Tween(begin: 0.0, end: 1.1);
+                    // return FadeTransition(
+                    //   opacity: animation.drive(tween),
+                    //   child: child,
+                    // );
+
+                    final tween = Tween(begin: 0.0, end: 1.0);
+                    return ScaleTransition(
+                      scale: animation.drive(tween),
+                      child: child,
+                    );
+                  }),
+                ),
+              );
+            },
+            child: Text("Go To About Screen"),
+          ),
+        ],
+      )),
+    );
+  }
+}
---
+++ 23_Flutter Animation/readme.md
@@ -0,0 +1,21 @@
+# (23) Flutter Animation
+
+## Data Diri
+
+Nomor Urut : 1_023FLB_52
+Nama : Ruslan
+
+## Summary
+
+Di section ini saya belajar tentang Flutter Animation yang meliputi :
+
+- Animation akan membuat aplikasi terlihat hidup yang berupa widget yang bergerak dari kondisi satu ke kondisi yang lainnya yang dapat menambah daya tarik pada aplikasi. Beberapa jenis animation yang ada di flutter diantaranya :
+
+  - Implicit Animation
+    Implicit animation adalah animasih dai widget yang sudah ada dan tanpa animation aplikasi akan terlihat tampak kaku dan dengan animation aplikasi akan terlihat lebih hidup
+
+  - Transition
+    Transition adalah animasi yang terjadi saat berpidah halaman yang dilakukan pada pagian navigator push, banyak animasi yang bisa digunakan dalam membuat aplikasi flutter
+
+  - Transation yang umum
+    Fade transition dan Scale transition merupakan animasi yang umum digunakan dalam sebuah aplikasi sehingga animasi ini sangat sering kita jumpai pada aplikasi aplikasi mobile yang ada.
